ce7d3278477efebd5e5c2c5e90726174d7448670
qianghe <qianghe421@163.com>
2016-11-05 22:09:36 +0800

    session-redis add

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# express-demo
+#express-demo
 
 ###简介
 
@@ -7,9 +7,17 @@
 ###使用组件及作用介绍如下:
 
 - express-session,浏览器会话管理
-- connect-redis
+- connect-redis,redisSession存储方案
 - mysql,数据库组件
 - log4js,日志组件
+- nodemon,热启
 
-###最新的一句话
+
+###redis服务端安装
+redis需要安装服务端,express中创建客户端建立连接,从而使用redis进行session会话存储
+(redis官网)[http://redis.io/download]
+(mac环境下安装:)[http://www.jianshu.com/p/6b5eca8d908b]
+(Linux&windows)[http://www.runoob.com/redis/redis-install.html]
+
+###最喜欢的一句话
 技术和工具永远只是实现想法的手段,工具可能会被更好的工具替代,但思考本身却需要自己不断地更新完善
--- app.js
+++ app.js
@@ -6,18 +6,27 @@
 var express = require('express');
 var app = express();
 var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var redisClient = require('./conf/redisClient');
 app.set('trust proxy', 1); // trust first proxy
+app.use(cookieParser());
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
+    store: redisClient,
+    secret: 'express-demo',
     saveUninitialized: true,
-    cookie: {secure: true}
+    resave: false
 }));
-var log = require('./conf/log');
-log.use(app);
+// var log = require('./conf/log');
+// log.use(app);
 // respond with "hello world" when a GET request is made to the homepage
+app.use(function (req, res, next) {
+    if (!req.session) {
+        return next(new Error('oh no')) // handle error
+    }
+    next(); // otherwise continue
+});
 app.get('/', function (req, res) {
-    res.send(req.session.id);
+    res.send(req.sessionID);
 });
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
