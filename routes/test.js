94c18f082205baebca96a64723088f79f5685053
alanhg <qianghe421@163.com>
2017-06-21 23:32:21 +0800

    读取文件渲染到模板实现

--- routes/test.js
+++ routes/test.js
@@ -4,6 +4,9 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/schema');
+const fs = require('fs');
+const path = require('path');
+const Base64 = require('js-base64').Base64;
 
 router.get('/css', function (req, res, next) {
     res.render('css');
@@ -14,4 +17,25 @@ router.get('/db', function (req, res) {
         res.send(data);
     });
 });
+
+router.get('/read', function (req, res) {
+    console.log(__dirname);
+    fs.readFile(path.join(__dirname, '../static/file/groups.json'), function (err, data) {
+        try {
+            let rs = JSON.parse(data);//wk推荐配置
+            // console.log(rs);
+            for (let group in rs) {
+                for (let item of rs[group]) {
+                    console.log(Base64.decode(item['TEXT']));
+                }
+            }
+            res.render('read', {data: rs, Base64: Base64});
+        } catch (e) {
+            console.log(e);
+            res.render('read', {data: [], Base64: Base64});
+        }
+    });
+
+});
+
 module.exports = router;
