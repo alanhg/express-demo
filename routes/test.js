d6e22ec6f05ebd89850d1dcb7c0d67f2d30f605c
alanhg <i@alanhe.me>
2018-01-02 23:53:27 +0800

    #1

--- routes/test.js
+++ routes/test.js
@@ -51,4 +51,88 @@ router.get('/c', function (req, res) {
     return res.send({value: conf2.a});
     console.log('hello');
 });
+
+
+router.get('/promise', function (req, res) {
+    var val = 1;
+
+// 我们假设step1, step2, step3都是ajax调用后端或者是
+// 在Node.js上查询数据库的异步操作
+// 每个步骤都有对应的失败和成功处理回调
+// 需求是这样，step1、step2、step3必须按顺序执行
+    function step1(resolve, reject) {
+        console.log('步骤一：执行');
+        if (val >= 1) {
+            resolve('Hello I am No.1');
+        } else if (val === 0) {
+            reject(val);
+        }
+    }
+
+    function step2(resolve, reject) {
+        console.log('步骤二：执行');
+        if (val === 1) {
+            resolve('Hello I am No.2');
+        } else if (val === 0) {
+            reject(val);
+        }
+    }
+
+    function step3(resolve, reject) {
+        console.log('步骤三：执行');
+        if (val === 1) {
+            resolve('Hello I am No.3');
+        } else if (val === 0) {
+            reject(val);
+        }
+    }
+
+    new Promise(step1).then(function (val) {
+        console.info(val);
+        return new Promise(step2);
+    }).then(function (val) {
+        console.info(val);
+        return new Promise(step3);
+    }).then(function (val) {
+        console.info(val);
+        return val;
+    });
+    return res.send({value: 'hello world'});
+});
+
+const superagent = require('superagent');
+
+router.get('/superagent', (req, res) => {
+    const getBaidu = () => {
+        return new Promise((resolve, reject) => {
+            superagent
+                .get('https://www.baidu.com/')
+                .end((err, res) => {
+                    // console.log(res);
+                    if (err) reject(err);
+                    resolve(res.text);
+                });
+        })
+    };
+    const getSina = () => {
+        return new Promise((resolve, reject) => {
+            superagent
+                .get('http://www.sina.com.cn/')
+                .end((err, res) => {
+                    // console.log(res);
+                    if (err) reject(err);
+                    resolve(res.text);
+                });
+        });
+    };
+    getBaidu().then((body) => {
+        console.log('百度内容');
+        // console.log(body);
+        return getSina();
+    }).then((body) => {
+        console.log('新浪内容');
+        // console.log(body);
+    })
+});
+
 module.exports = router;
