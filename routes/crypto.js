4029377819088fc1271505b9d9b80a2875371ff2
Alan He <qianghe421@gmail.com>
2022-03-05 22:14:40 +0800

    test crypto.js

--- routes/crypto.js
+++ routes/crypto.js
@@ -1,15 +1,26 @@
 const forge = require('node-forge');
 const fs = require('fs');
+const crypto = require('crypto');
 
 
 // var pubHex = rs.KEYUTIL.getHexFro∂mPEM(pubPEM);
 // var pubHex = rs..getHexFromPEM(a.asn1.x509.X509Util.getPKCS8PubKeyPEMfromRSAKey(prvKey));
 
-const privateKey = fs.readFileSync(`${__dirname}/client1.key`, {
+const privateKeyPem = fs.readFileSync(`${__dirname}/client1.key`, {
   encoding: 'utf8'
 });
 
-const forgePriKey = forge.pki.privateKeyFromPem(privateKey);
+const forgePriKey = forge.pki.privateKeyFromPem(privateKeyPem);
 const forgePubKey = forge.pki.setRsaPublicKey(forgePriKey.n, forgePriKey.e);
-const publicKey = forge.pki.publicKeyToPem(forgePubKey);
-console.log(publicKey);
+const publicKeyPem = forge.pki.publicKeyToPem(forgePubKey);
+console.log(publicKeyPem);
+
+(function () {
+  const pub = crypto.createPublicKey(privateKeyPem);
+  const pubstr = pub.export({
+    format: 'pem',
+    type: 'spki'
+  })
+  console.log('by nodejs')
+  console.log(pubstr);
+})();
