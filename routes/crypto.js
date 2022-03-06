e899c4831243ed713e11c41ce10314ee41d2b644
alanhe <qianghe421@gmail.com>
2022-03-06 12:32:20 +0800

    test openssl

--- routes/crypto.js
+++ routes/crypto.js
@@ -1,6 +1,10 @@
+/**
+ * 测试基于私钥导出公钥
+ */
 const forge = require('node-forge');
 const fs = require('fs');
 const crypto = require('crypto');
+const {execSync} = require('child_process');
 
 
 // var pubHex = rs.KEYUTIL.getHexFro∂mPEM(pubPEM);
@@ -13,14 +17,20 @@ const privateKeyPem = fs.readFileSync(`${__dirname}/client1.key`, {
 const forgePriKey = forge.pki.privateKeyFromPem(privateKeyPem);
 const forgePubKey = forge.pki.setRsaPublicKey(forgePriKey.n, forgePriKey.e);
 const publicKeyPem = forge.pki.publicKeyToPem(forgePubKey);
+console.log('by node-forge');
 console.log(publicKeyPem);
 
 (function () {
   const pub = crypto.createPublicKey(privateKeyPem);
   const pubstr = pub.export({
-    format: 'pem',
-    type: 'spki'
+    format: 'pem', type: 'spki'
   })
   console.log('by nodejs')
   console.log(pubstr);
 })();
+
+
+(function () {
+  console.log('by openssl')
+  execSync(`openssl pkey -in ${__dirname}/client1.key -pubout`, {stdio: 'inherit'})
+})()
