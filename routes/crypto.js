8aed389a228e652b5536a05de1d5bd9d68f7be33
alanhe <qianghe421@gmail.com>
2022-04-10 22:42:27 +0800

    补充test文件

--- routes/crypto.js
+++ routes/crypto.js
@@ -10,6 +10,8 @@ const {Base64} = require('js-base64');
 const {KEYUTIL, KJUR} = require("jsrsasign");
 const buffer = require("buffer");
 const Base58 = require("base-58")
+const querystring = require("querystring")
+const keccak256 = require('keccak256')
 
 // var pubHex = rs.KEYUTIL.getHexFro∂mPEM(pubPEM);
 // var pubHex = rs..getHexFromPEM(a.asn1.x509.X509Util.getPKCS8PubKeyPEMfromRSAKey(prvKey));
@@ -50,20 +52,7 @@ const getPemBody = (pem) => {
   execSync(`openssl pkey -in ${__dirname}/client1.key -outform der –out > ${__dirname}/client1.der`, {stdio: 'inherit'})
 })();
 
-
-// const hashData = sm3(.decode(()));
-//
-// function toHex(str) {
-//   let result = '';
-//   for (let i = 0; i < str.length; i++) {
-//     result += str.charCodeAt(i).toString(16);
-//   }
-//   return result;
-// }
-//
-// let str = hashData.substring(0, 20);
-
-(function sha256Pem() {
+(function addrCal() {
   let pemStr = `MIIBCgKCAQEA6bYlyT3p2mieVwYsuNJSXJSrGUcyZQdU0EN3qcGkqssoNVkZz8km
 eAYri0LH8EWPrQX8t67FlUsczChsmjK+tiBotxBZ18CFD47bDFjjOoAb4Qhhgyuc
 JIFRGZNXLPl2yUBA3Hpn/J4Q02DpnH9t9zqCihS/f2Zt5o8G/ssgz/Wd7rMwQon2
@@ -80,16 +69,7 @@ FGgGcz/s/LVc+KxYlQTSK4aLHSXwzxuOVwIDAQAB`
   const hash = crypto.createHash('sha256');
   hash.update(buff);
   let str = hash.digest('hex');
-  console.log('sha256 address[nodejs]', str);
-  const res = Base58.encode(Buffer.from(str, 'hex'));
-  console.log('address[nodejs]', res);
+  console.log(keccak256(str).toString('hex'));
+  // const res = Base58.encode(Buffer.from(str, 'hex'));
+  // console.log('address[nodejs]', res);
 })();
-
-//
-// (function getAddressFromPublicKey() {
-//   const publicKey = '' + 'MIIBCgKCAQEA6bYlyT3p2mieVwYsuNJSXJSrGUcyZQdU0EN3qcGkqssoNVkZz8km\n' + 'eAYri0LH8EWPrQX8t67FlUsczChsmjK+tiBotxBZ18CFD47bDFjjOoAb4Qhhgyuc\n' + 'JIFRGZNXLPl2yUBA3Hpn/J4Q02DpnH9t9zqCihS/f2Zt5o8G/ssgz/Wd7rMwQon2\n' + 'MYceDa9Bla8mU1WA/dmDL/zKEekZvlxUWuhExdIERLE3RgxX1MXL4fCqO0x2ySkS\n' + 'xxL5kKaHe7qHRkF1F7tswwwJN4w3TVOwlhjKnjR5zvw2tGOwaVfv5Ujym0bNfx6n\n' + 'FGgGcz/s/LVc+KxYlQTSK4aLHSXwzxuOVwIDAQAB';
-//   const md = new KJUR.crypto.MessageDigest({alg: "sha256"});
-//   md.updateString(publicKey);
-//   const mdHex = md.digest();
-//   console.log('cal address', mdHex);
-// })();
