265997b708c5e1187f5f377c6373260a63646fff
alanhe <qianghe421@gmail.com>
2022-07-03 23:25:35 +0800

    编码测试

--- routes/encode.js
+++ routes/encode.js
@@ -2,7 +2,7 @@
  * 测试基于私钥导出公钥
  */
 
-const szStart = Buffer.from('rz\r**\x18B00000000000000\r\x8a');
-const szStart2 = Buffer.from('rz\r**B00000000000000\r�');
+const szStart = Buffer.from('rz\r**\x18B00000000000000\r');
+const szStart2 = Buffer.from('rz\r**B00000000000000\r');
 
 console.log(szStart.toString() === szStart2.toString());
