e5262b191e2be0b1c1ff7dda67e7154967f1ad45
alanhe <qianghe421@gmail.com>
2022-07-04 22:42:21 +0800

    升级xterm-addon-search，xterm-addon-web-links

---
+++ build.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const path = require('path');
+fs.copyFileSync(path.join(__dirname, 'node_modules', 'xterm-addon-search', 'lib/xterm-addon-search.js'), path.join(__dirname, 'static', 'js/xterm-addon-search.js'));
+fs.copyFileSync(path.join(__dirname, 'node_modules', 'xterm-addon-web-links', 'lib/xterm-addon-web-links.js'), path.join(__dirname, 'static', 'js/xterm-addon-web-links.js'));
