838b80880ed20b2997b4c4aa0e791d2cb83fa451
alanhe <qianghe421@gmail.com>
2022-10-08 07:31:17 +0800

    sshAgent

--- routes/code-server.js
+++ routes/code-server.js
@@ -4,7 +4,12 @@
  */
 const express = require("express");
 const axios = require("axios");
+const {HTTPAgent} = require("ssh2");
 const router = express.Router();
+
+const httpAgent = new HTTPAgent({
+  host: process.env.host, port: 22, username: process.env.username || 'root', password: process.env.password
+});
 
 /**
  * http://127.0.0.1:8000/ws/1665154822948?folder=/home/lighthouse/.ssh
@@ -14,18 +19,23 @@ const router = express.Router();
 router.all('/:id/?*', (req, res) => {
   let url = req.url.replace(/^\/\d*/, '');
   const headers = Object.keys(req.headers).reduce((h, key) => {
-    h[key.toLowerCase()] = req.headers[key].replace(/127.0.0.1:8000(\/ws\/\d+)?/, 'localhost:8002');
+    h[key.toLowerCase()] = req.headers[key].replace(/127.0.0.1:8000(\/ws\/\d+)?/, 'localhost:8080');
     return h;
   }, {});
   const bodyData = Object.keys(req.body).reduce((h, key) => {
     if (!req.body[key]) {
       return h;
     }
-    h[key] = req.body[key].replace(/127.0.0.1:8000(\/ws\/\d+)?/, 'localhost:8002');
+    h[key] = req.body[key].replace(/127.0.0.1:8000(\/ws\/\d+)?/, 'localhost:8080');
     return h;
   }, {});
+
+  /**
+   * 根据ID确定需要连接的httpAgent
+   */
   axios({
-    headers, baseURL: 'http://localhost:8002', method: req.method, url, data: bodyData,
+    headers, baseURL: 'http://localhost:8080', method: req.method, url, data: bodyData,
+    httpAgent
   }).then(response => {
     res.set(response.headers);
     res.send(response.data);
