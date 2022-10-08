e37d37ef25c7d653fc14b1f13de1a7beb1ab39cb
Alan He <qianghe421@gmail.com>
2022-10-08 17:25:44 +0800

    optimize

--- test/axios-agent.js
+++ test/axios-agent.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 var querystring = require('querystring');
+const WebSocket = require("ws");
+const {codeServerProxifier} = require("../lib/code-server/code-server-proxy");
 
 // axios({
 //   url: 'http://localhost:8002', maxRedirects: 0
@@ -13,18 +15,32 @@ let obj = {
   base: '.', href: 'http://localhost:8002/login', password: '1b4cae37fa18eac9384af396'
 };
 
-axios({
-  baseURL: 'http://localhost:8002', url: '/login', method: 'post', headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  }, maxRedirects: 0,
-  data: querystring.stringify(obj),
-}).then(response => {
-  console.log(response.status);
-  console.log(response.data);
-}).catch(e => {
-  console.log(e.response.status);
-});
+// axios({
+//   baseURL: 'http://localhost:8002', url: '/login', method: 'post', headers: {
+//     "Content-Type": "application/x-www-form-urlencoded"
+//   }, maxRedirects: 0,
+//   data: querystring.stringify(obj),
+// }).then(response => {
+//   console.log(response.status);
+//   console.log(response.data);
+// }).catch(e => {
+//   console.log(e.response.status);
+// });
 
+
+const targetWs = new WebSocket(`ws://127.0.0.1:8002/stable-74b1f979648cc44d385a2286793c226e611f59e7?reconnectionToken=32b52a08-155c-4ab9-866a-3d7e770c754a&reconnection=false&skipWebSocketFrames=false`, {});
+targetWs.on('open', () => {
+  console.log('socket open');
+});
+targetWs.on('error', function (err) {
+  console.log('socket error', err);
+});
+targetWs.on('close', function () {
+  console.log('socket close');
+});
+targetWs.on('message', (msg2) => {
+  console.log('socket receive msg', msg2);
+});
 
 //
 // const http = require('http');
@@ -54,3 +70,4 @@ axios({
 // }).on("error", (err) => {
 //   console.log("Error: ", err)
 // }).end()
+
