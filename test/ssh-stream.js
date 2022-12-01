878208c373fd7c9302b2d096b87d3f6d9f20e6c3
Alan He <qianghe421@gmail.com>
2022-12-01 21:52:12 +0800

    optimize

--- test/ssh-stream.js
+++ test/ssh-stream.js
@@ -16,19 +16,40 @@ const path = require("path");
 let bufferLength = 5;
 let offset = 0;
 
+
 class Throttle extends Stream.Transform {
+  constructor(opts = {}) {
+    super(opts);
+    this.chunkSize = opts.chunkSize;
+    this.bufTemp = Buffer.alloc(0);
+  }
+
   _transform(buf, enc, next) {
-    this.push(buf)
-    next()
+    if (!this.chunkSize) {
+      this.push(buf);
+    } else {
+      this.bufTemp = Buffer.concat([this.bufTemp, buf]);
+      if (this.bufTemp.length >= this.chunkSize) {
+        this.push(this.bufTemp.slice(0, this.chunkSize));
+        this.bufTemp = this.bufTemp.slice(this.chunkSize);
+      }
+    }
+    next();
+  }
+
+  _flush(next) {
+    if (this.chunkSize && this.bufTemp.length) {
+      this.push(this.bufTemp);
+      this.bufTemp = Buffer.alloc(0);
+    }
+    next();
   }
 }
 
-const writableStream = new Throttle();
+const writableStream = new Throttle({chunkSize: 100});
 
 writableStream.on('data', data => {
-  console.log('---');
   console.log(data.toString());
-  console.log('---');
 })
 
 // 所有数据均已读完
@@ -36,64 +57,64 @@ writableStream.on('end', () => {
   console.log('🎉done');
 })
 
-// let interval = setInterval(() => {
-//   // 接收前端binaryData
-//   const buf = Buffer.from(offset.toString(), "utf-8");
-//   // writableStream.write(Buffer.from([104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]));
-//   offset = offset + buf.byteLength;
-//   if ((offset + buf.byteLength) > bufferLength) {
-//     clearInterval(interval);
-//     writableStream.end();
-//   }
-// }, 1000);
-const dataStr = BSON.serialize({a: Buffer.from('112323')});
-const dataParsed = BSON.deserialize(dataStr);
-// console.log('Class: , Function: , Line 51, Param: ', dataParsed.a.buffer);
-
-// writableStream.write(dataParsed.a.buffer);
-
-let uploadStream = null;
-
-const client = {
-  connect: () => {
-    setTimeout(() => {
-      uploadStream.write(Buffer.from(Math.random().toString()));
-      uploadStream.on('data', (buf) => {
-        console.log(buf.byteLength);
-      })
-    }, 3000);
-  }, upload: () => {
-    uploadStream = new Throttle();
-    // const res = uploadStream.write(Buffer.from(Math.random().toString()));
-    const res = uploadStream.write(Buffer.from([104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]));
-    console.log('Class: client, Function: upload, Line 61, Param: res', res);
-
-    uploadStream.on('data', (buf) => {
-      console.log(buf.toString());
-    })
+let interval = setInterval(() => {
+  // 接收前端binaryData
+  const buf = Buffer.from(offset.toString(), "utf-8");
+  writableStream.write(Buffer.from([104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]));
+  offset = offset + buf.byteLength;
+  if ((offset + buf.byteLength) > bufferLength) {
+    clearInterval(interval);
+    writableStream.end();
   }
-}
-
-// client.connect();
-// client.upload();
-
-const input = 'drwxr-xr-x 3 root root 4096 Jun 05 22:01 .local';
-
-
-const owner = input ? input.match(/(\d+\s)(\S+)/)[0].replace(/\d+\s/, '') : '';
-
-// console.log(owner);
-
-
-let now = new Date();
-let data1 = JSON.stringify({a: now});
-// console.log(data1);
-
-// console.log(typeof JSON.parse(data1).a);
-
-// console.log('Class: , Function: , Line 91, Param: ', Buffer.from(JSON.parse(data1).a).toString());
-
-let data2 = BSON.serialize({a: now});
-console.log(Buffer.from(data1).byteLength);
-console.log(data2.byteLength);
+}, 1000);
+// const dataStr = BSON.serialize({a: Buffer.from('112323')});
+// const dataParsed = BSON.deserialize(dataStr);
+// // console.log('Class: , Function: , Line 51, Param: ', dataParsed.a.buffer);
+//
+// // writableStream.write(dataParsed.a.buffer);
+//
+// let uploadStream = null;
+//
+// const client = {
+//   connect: () => {
+//     setTimeout(() => {
+//       uploadStream.write(Buffer.from(Math.random().toString()));
+//       uploadStream.on('data', (buf) => {
+//         console.log(buf.byteLength);
+//       })
+//     }, 3000);
+//   }, upload: () => {
+//     uploadStream = new Throttle();
+//     // const res = uploadStream.write(Buffer.from(Math.random().toString()));
+//     const res = uploadStream.write(Buffer.from([104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100]));
+//     console.log('Class: client, Function: upload, Line 61, Param: res', res);
+//
+//     uploadStream.on('data', (buf) => {
+//       console.log(buf.toString());
+//     })
+//   }
+// }
+//
+// // client.connect();
+// // client.upload();
+//
+// const input = 'drwxr-xr-x 3 root root 4096 Jun 05 22:01 .local';
+//
+//
+// const owner = input ? input.match(/(\d+\s)(\S+)/)[0].replace(/\d+\s/, '') : '';
+//
+// // console.log(owner);
+//
+//
+// let now = new Date();
+// let data1 = JSON.stringify({a: now});
+// // console.log(data1);
+//
+// // console.log(typeof JSON.parse(data1).a);
+//
+// // console.log('Class: , Function: , Line 91, Param: ', Buffer.from(JSON.parse(data1).a).toString());
+//
+// let data2 = BSON.serialize({a: now});
+// console.log(Buffer.from(data1).byteLength);
+// console.log(data2.byteLength);
 
