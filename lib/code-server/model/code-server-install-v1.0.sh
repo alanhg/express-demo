22bd422480fec9ad5e7aa7f11c382bed39b81559
Alan He <qianghe421@gmail.com>
2022-12-06 21:23:18 +0800

    optimize

--- lib/code-server/model/code-server-install-v1.0.sh
+++ lib/code-server/model/code-server-install-v1.0.sh
@@ -86,7 +86,7 @@ fi
 
  start_supervisor_server(){
   echo '5. start supervisor server'
-  $CODE_SERVER_RUN_DIR/supervisord-conf/bin/supervisord -c $CODE_SERVER_RUN_DIR/supervisord-conf/supervisord.conf ctl reload
+  $CODE_SERVER_RUN_DIR/supervisord-conf/bin/supervisord -c $CODE_SERVER_RUN_DIR/supervisord-conf/supervisord.conf -d
   # $CODE_SERVER_RUN_DIR/supervisord-conf/bin/supervisord -c $CODE_SERVER_RUN_DIR/supervisord-conf/supervisord.conf ctl status code-server
   client_status=$($CODE_SERVER_RUN_DIR/supervisord-conf/bin/supervisord -c $CODE_SERVER_RUN_DIR/supervisord-conf/supervisord.conf ctl restart code-server)
 
