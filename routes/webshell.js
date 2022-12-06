711bae76a103ff809c05b72b4e09f85ef2c15ec1
Alan He <qianghe421@gmail.com>
2022-12-06 15:39:21 +0800

    optimize

--- routes/webshell.js
+++ routes/webshell.js
@@ -13,9 +13,13 @@ let logStartFlag = false;
 let shellLog;
 
 const connectOpts = {
-  host: process.env.host, port: 22, username: process.env.username || 'root', password: process.env.password,
+  host: process.env.host,
+  port: process.env.port || 22,
+  username: process.env.username || 'root',
+  password: process.env.password,
   keepaliveInterval: 30000,
   keepaliveCountMax: 200,
+  debug: console.log
 };
 
 router.ws('/ws/webshell', function (ws, res) {
