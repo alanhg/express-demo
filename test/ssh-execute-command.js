82dac75024f5727daf182370fdbde1b216437150
Alan He <qianghe421@gmail.com>
2022-12-08 16:03:10 +0800

    update

--- test/ssh-execute-command.js
+++ test/ssh-execute-command.js
@@ -3,14 +3,14 @@
 const path = require('path');
 const {readFileSync} = require('fs');
 const {Client} = require('ssh2');
-let privateKey = readFileSync(path.join(process.env.HOME, '.ssh/alan_home.cer'));
 // const ssh2Streams = require('ssh2-streams');
 // const {KEX, CIPHER, SUPPORTED_SERVER_HOST_KEY} = ssh2Streams.constants.ALGORITHMS;
 // const kex = KEX.concat(['diffie-hellman-group1-sha1']);
 // const cipher = CIPHER.concat(['aes256-cbc', 'aes192-cbc', 'aes128-cbc']);
 
 const config = {
-  host: '', port: 22, username: 'root', tryKeyboard: true, algorithms: {
+  host: process.env.host,
+  port: process.env.port, username: process.env.username, tryKeyboard: true, algorithms: {
     kex: ['curve25519-sha256', 'curve25519-sha256@libssh.org', 'ecdh-sha2-nistp256', 'ecdh-sha2-nistp384', 'ecdh-sha2-nistp521', // 'sntrup761x25519-sha512@openssh.com',
       'diffie-hellman-group-exchange-sha256', 'diffie-hellman-group16-sha512', 'diffie-hellman-group18-sha512', 'diffie-hellman-group14-sha256'],
     cipher: [// 'chacha20-poly1305@openssh.com',
@@ -22,7 +22,7 @@ const config = {
       'rsa-sha2-256', 'rsa-sha2-512', 'ssh-dss', 'ecdsa-sha2-nistp256', 'ecdsa-sha2-nistp384', 'ecdsa-sha2-nistp521', // 'sk-ecdsa-sha2-nistp256@openssh.com',
       // 'webauthn-sk-ecdsa-sha2-nistp256@openssh.com'
     ]
-  }, password: ''
+  }, password: process.env.password
 };
 
 
@@ -33,8 +33,11 @@ async function main() {
     console.log(err);
   }).on('ready', () => {
     console.log('connected');
-    sshClient.exec('echo123 https://1991421.cn && exit 1', (err, stream) => {
-      if (err) throw err;
+    sshClient.exec('/usr/bin/caddy start --config .term/code-server-run/caddy/Caddyfile\n', (err, stream) => {
+      if (err) {
+        console.error(err);
+        throw err;
+      }
       stream.on('close', (code, signal) => {
         // 如果正常结束，code为0，
         console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
@@ -43,6 +46,7 @@ async function main() {
         console.log('STDOUT: ' + data);
       }).stderr.on('data', (data) => {
         console.log('STDERR: ' + data);
+        stream.close();
       });
     });
   });
