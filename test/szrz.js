6a8d0267879d095b1a072a34251eaf65326dd32a
Alan He <qianghe421@gmail.com>
2022-12-13 23:16:20 +0800

    optimize

--- test/szrz.js
+++ test/szrz.js
@@ -1,5 +1,19 @@
-const {Base64} = require('js-base64');
+// const {Base64} = require('js-base64');
+//
+// let config = {username: 'root', host: '121.4.29.15'};
+//
+// console.log(Base64.isValid('123'));
 
-let config = {username: 'root', host: '121.4.29.15'};
 
-console.log(Base64.encode(config.host + config.username));
+function generatePassword() {
+  var length = 20,
+    charset =
+      "@#$&*0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$&*0123456789abcdefghijklmnopqrstuvwxyz",
+    password = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    password += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return password;
+}
+
+console.log(generatePassword());
