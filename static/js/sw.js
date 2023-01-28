286b5e9f599077cc085336c3f9cfa2a9341f36bf
alanhe <qianghe421@gmail.com>
2023-01-28 11:41:58 +0800

    test pwa

---
+++ static/js/sw.js
@@ -0,0 +1,8 @@
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetched resource ' + e.request.url);
+});
