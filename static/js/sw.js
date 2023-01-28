3dba1b36f6025381242ba74254429660c54f0900
alanhe <qianghe421@gmail.com>
2023-01-28 14:53:36 +0800

    optimize

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,8 +1,12 @@
 // Installing Service Worker
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+
+const CACHE_NAME = 'static_assets_v1';
+const urlsToCache = ['/js/xterm/css/xterm.css', '/js/contextMenu.css', '/js/xterm/lib/xterm.js'];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetched resource ' + e.request.url);
+self.addEventListener('fetch', () => {
 });
