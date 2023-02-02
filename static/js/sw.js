4916451dee741c6c29b9753db44d7eee5e4fdc5f
alanhe <qianghe421@gmail.com>
2023-02-02 23:56:10 +0800

    sw

--- static/js/sw.js
+++ static/js/sw.js
@@ -8,7 +8,7 @@ const urlsToCache = ['/js/xterm/css/xterm.css', '/js/contextMenu.css', '/js/xter
  */
 self.addEventListener('install', function (event) {
   // Perform install steps
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  // event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', () => {
@@ -17,17 +17,17 @@ self.addEventListener('fetch', () => {
 
 self.addEventListener('activate', function (e) {
   console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
-        if (key !== CACHE_NAME) {
-          // 清理旧版本
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+  // e.waitUntil(
+  //   caches.keys().then(function (keyList) {
+  //     return Promise.all(keyList.map(function (key) {
+  //       if (key !== CACHE_NAME) {
+  //         // 清理旧版本
+  //         console.log('[ServiceWorker] Removing old cache', key);
+  //         return caches.delete(key);
+  //       }
+  //     }));
+  //   })
+  // );
   // 更新客户端
   return self.clients.claim();
 });
