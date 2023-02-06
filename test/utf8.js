f447f5104d69bd8ba949fd8c525b530ee7b969a6
Alan He <qianghe421@gmail.com>
2023-02-06 14:48:48 +0800

    aes加解密支持

--- test/utf8.js
+++ test/utf8.js
@@ -1,20 +1,6 @@
-console.log( `
-      renew_password() {
-    if [ -e $ORCA_CODE_SERVER_DIR/.config/config.yaml ];then
-      cat <<EOF >  $ORCA_CODE_SERVER_DIR/.config/config.yaml
-bind-addr: 127.0.0.1:8080
-auth: password
-cert: false
-EOF
-    fi
-      }
-      
-client_status=$($ORCA_CODE_SERVER_RUN_DIR/supervisord-conf/bin/supervisord -c $ORCA_CODE_SERVER_RUN_DIR/supervisord-conf/supervisord.conf ctl status code-server)
-client_status=$(echo "$client_status" | tr '[:upper:]' '[:lower:]')
+const {aesDecrypt, aesEncrypt} = require('../lib/utils');
+const encryptedMsg = aesEncrypt('123');
+const msg = aesDecrypt(encryptedMsg);
 
-if test -n "\${client_status#*stopped}"; then
- renew_password
-elif [ "$client_status" = "" ]; then
- renew_password
- fi
-    `)
+console.log(encryptedMsg);
+console.log(msg);
