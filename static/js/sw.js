c129ffabc49757302bf3350c607c4dec71a1f29f
alanhe <qianghe421@gmail.com>
2023-02-27 22:00:08 +0800

    update sw.js

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,14 +1,8 @@
 // Installing Service Worker
 
 const CACHE_NAME = 'static_assets_v1';
-const urlsToCache = ['/js/xterm/css/xterm.css', '/js/contextMenu.css', '/js/xterm/lib/xterm.js'];
 
-/**
- * install事件一般是被用来填充浏览器的离线缓存能力
- */
 self.addEventListener('install', function (event) {
-  // Perform install steps
-  // event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', () => {
@@ -17,26 +11,17 @@ self.addEventListener('fetch', () => {
 
 self.addEventListener('activate', function (e) {
   console.log('[ServiceWorker] Activate');
-
-  // e.waitUntil(
-  //   caches.keys().then(function (keyList) {
-  //     return Promise.all(keyList.map(function (key) {
-  //       if (key !== CACHE_NAME) {
-  //         // 清理旧版本
-  //         console.log('[ServiceWorker] Removing old cache', key);
-  //         return caches.delete(key);
-  //       }
-  //     }));
-  //   })
-  // );
-  event.waitUntil(async function() {
-      // 发送消息到网页
-    const allClients = await self.clients.matchAll();
-    allClients.forEach(client => {
-      client.postMessage({ type: 'service-worker-activated' });
-    });
-    console.log('[Service Worker] Matching clients:', allClients);
-  }());
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== CACHE_NAME) {
+          // 清理旧版本
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
   // 更新客户端
   return self.clients.claim();
 });
