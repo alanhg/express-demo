061c37c6b5efe172f879ea1ea716ea57693b3089
Alan He <qianghe421@gmail.com>
2023-02-27 17:03:13 +0800

    just test

--- test/utf8.js
+++ test/utf8.js
@@ -1,6 +1,35 @@
 const {aesDecrypt, aesEncrypt} = require('../lib/utils');
+const pt = require('path');
+const fs = require('fs');
+const https = require('https');
 const encryptedMsg = aesEncrypt('123');
 const msg = aesDecrypt(encryptedMsg);
 
 console.log(encryptedMsg);
 console.log(msg);
+
+const pwaFile = pt.join(__dirname, 'pwa', 'a.txt');
+
+
+async function main() {
+  await new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(pwaFile);
+    https.get(
+        'https://1991421.cn',
+        (response) => {
+          if (response.statusCode === 200) {
+            response.pipe(file);
+            // after download completed close filestream
+            file.on('finish', () => {
+              file.close();
+              resolve();
+            });
+          } else {
+            reject(new Error(`Failed to download file, status code: ${response.statusCode}`));
+          }
+        },
+    );
+  });
+}
+
+main();
