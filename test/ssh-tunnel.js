b5288bab8666463113c9eff438f87631edf93189
Alan He <qianghe421@gmail.com>
2023-09-06 16:57:09 +0800

    test axios error

--- test/ssh-tunnel.js
+++ test/ssh-tunnel.js
@@ -1,5 +1,7 @@
 const {Client} = require('ssh2');
-const net = require("net");
+const net = require('net');
+const {post} = require('axios');
+const axios = require('axios');
 
 /**
  * 希望于能够SSH联通目标机器，代理目标机器的某WEB服务，
@@ -13,22 +15,35 @@ const net = require("net");
  */
 const conn = new Client();
 // Send a raw HTTP request to port 80 on the server
-conn.on('ready', () => {
-  console.log('FIRST :: connection ready');
+// conn.on('ready', () => {
+//   console.log('FIRST :: connection ready');
+//
+//   net.createServer(function(sock) {
+//     // may want to sock.pause() first if on an older (pre-v0.10-ish?) node version
+//     conn.forwardOut(sock.remoteAddress, sock.remotePort, '127.0.0.1', 8000,  function(err, stream) {
+//       if (err) throw err; // do something better than this
+//
+//       sock.pipe(stream);
+//       stream.pipe(sock);
+//
+//       // sock.resume() here if you paused earlier
+//     });
+//   }).listen(8000);
+// }).connect({
+//   host: process.env.host, port: process.env.port, username: process.env.username, password: process.env.password
+// });
 
-  net.createServer(function(sock) {
-    // may want to sock.pause() first if on an older (pre-v0.10-ish?) node version
-    conn.forwardOut(sock.remoteAddress, sock.remotePort, '127.0.0.1', 8000,  function(err, stream) {
-      if (err) throw err; // do something better than this
-
-      sock.pipe(stream);
-      stream.pipe(sock);
-
-      // sock.resume() here if you paused earlier
-    });
-  }).listen(8000);
-}).connect({
-  host: process.env.host, port: process.env.port, username: process.env.username, password: process.env.password
+axios.get('https://1991421.cn/123about', {
+  headers: {},
+}).then(res => {
+  console.log(res);
+}).catch(e => {
+  if (e.isAxiosError) {
+    console.error(e.response.data);
+    console.error(e.response.status);
+  } else {
+    console.error(e.message);
+  }
 });
 
 
