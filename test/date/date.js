8370bb0690f277734abf7b99fcae70db403b369d
Alan He <qianghe421@gmail.com>
2023-09-11 12:32:40 +0800

    test date utils

---
+++ test/date/date.js
@@ -0,0 +1,6 @@
+const moment = require('moment');
+const defaultStartDate = moment().startOf('day');
+const quotaEndDate = defaultStartDate.clone().add(1, 'days');
+
+console.log(defaultStartDate, quotaEndDate);
+// console.log(defaultStartDate.toDate());
