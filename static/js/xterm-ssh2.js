5efac326321bde4ff7ec8253773c6c5d4692a4cd
Alan He <qianghe421@gmail.com>
2023-12-10 20:13:22 +0800

    init

--- static/js/xterm-ssh2.js
+++ static/js/xterm-ssh2.js
@@ -5,6 +5,12 @@ import {SearchAddonBar} from '/js/search-addon-bar.js';
 import {RecordScreenAddon} from '/js/record-screen-addon.js';
 
 const textEncoder = new TextEncoder();
+const fontSizeOptions = [12, 14, 16, 18, 20];
+const webSocketBaseUrl = `${location.protocol === `https:` ? 'wss:' : 'ws:'}//${location.host}`;
+const CurrentDirRecord = [];
+const isMac = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
+const isWin = navigator.platform.indexOf('Win') > -1;
+
 
 class WebShell extends EventEmitter {
 
@@ -52,7 +58,7 @@ class WebShell extends EventEmitter {
            * folder参数控制缺省打开文件夹,不传则走前端存储的最后打开文件夹
            * ?folder=/home
            */
-          let codeServerWeb = document.getElementById('visitIDEDirectly').checked ? (`/tty/${options.proxyKey}/`) : (`/ide/${options.proxyKey}/`);
+          let codeServerWeb = document.getElementById('visitIDEDirectly').checked ? (`/tty/${options.data.proxyKey}/`) : (`/ide/${options.data.proxyKey}/`);
           console.log(`访问地址：${window.location.protocol}//${window.location.host}${codeServerWeb}`);
           if (window.toggleNewTabOn.checked) {
             window.open(codeServerWeb, '_blank');
@@ -124,7 +130,8 @@ term = new Terminal({
 });
 window.term = term;
 const webshell = new WebShell(term);
-webshell.connect(JSON.parse(document.getElementById('proxyHost').value));
+webshell.connect(window.connectOpts);
+window.webshell = webshell;
 
 
 /**
@@ -883,10 +890,203 @@ $('#terminal-container').contextmenu(function (e) {
   ContextMenu.render(e, menu, this); //开始渲染
 });
 
-document.querySelector('#executeCommandBtn').onclick= function () {
+document.querySelector('#executeCommandBtn').onclick = function () {
   let textEl = document.getElementById('executeCommand');
-  webshell.execCommand(textEl.value).then(({data})=>{
-    textEl.value=data;
+  webshell.execCommand(textEl.value).then(({data}) => {
+    textEl.value = data;
   });
 }
 
+
+document.getElementById('proxyHost').value = JSON.stringify(connectOpts, null, 2);
+
+
+/**
+ * 测试高频打印数据
+ */
+function onPrintBtnClick() {
+  let index = 0;
+  window.term.blur();
+  window.term.writeln('');
+  const intervalId = setInterval(() => {
+    window.term.write((index++) + '\r');
+    if (index > 10000000) {
+      clearInterval(intervalId);
+    }
+  })
+}
+
+function onInfinitePrintBtnClick() {
+  let index = 0;
+  while (true) {
+    window.term.write((index++) + '\r');
+  }
+}
+
+function recordLogStart(event) {
+
+  event.target.innerText = event.target.dataset.start === 'true' ? 'log record start' : 'log record stop';
+  event.target.dataset.start = event.target.dataset.start !== 'true';
+
+  if (event.target.dataset.start === 'true') {
+    window.recordScreenAddon.start();
+  } else {
+    window.recordScreenAddon.stop();
+  }
+
+  fetch(`/ssh2-log?start=${event.target.dataset.start}&&recordTimestamp=${document.getElementById('record-timestamp').checked}`, {
+    method: 'get',
+  }).then(() => {
+    if (event.target.dataset.start === 'false') {
+      const anchorElement = document.createElement('a');
+      anchorElement.download = 'ssh-log-record.log';
+      anchorElement.href = '/logs/ssh-log-record_plaintext.log';
+      anchorElement.click();
+    }
+  });
+}
+
+
+function serializeTerminal(event) {
+  console.log(serializeAddon.serialize());
+}
+
+function clientRecordLogStart(event) {
+  event.target.innerText = event.target.dataset.start === 'true' ? 'client log record start' : 'client log record stop';
+  event.target.dataset.start = event.target.dataset.start !== 'true';
+  clientRecordLogFlag = event.target.dataset.start === 'true';
+  if (clientRecordLogFlag) {
+    clientRecordLogBlobs = [];
+  } else {
+    let htmlAnchorElement = document.createElement('a');
+    htmlAnchorElement.style.display = 'hidden';
+    htmlAnchorElement.download = Date.now() + '.log';
+    htmlAnchorElement.href = URL.createObjectURL(new Blob(clientRecordLogBlobs));
+    htmlAnchorElement.click();
+  }
+}
+
+function runCodeServer(e) {
+  sendData('codeserver', {
+    proxyProtocol: document.getElementById('proxyProtocol').value
+  })
+}
+
+function runWebSocketProxy() {
+  sendData('websocket-proxy', {});
+
+}
+
+function stopCodeServer(e) {
+  sendData('codeserver-logout')
+}
+
+function toggleBackgroundOn(e) {
+  if (e.target.checked) {
+    document.querySelector('.terminal-background-img').style.backgroundImage = `url("/images/1db903d5-1fe8-4737-8f6b-2fbe20fbe9f4.png")`;
+  } else {
+    document.querySelector('.terminal-background-img').style.backgroundImage = '';
+  }
+}
+
+function toggleNewTabOn(e) {
+  window.toggleNewTabOn = e.target.checked;
+}
+
+const SPACER = '            ';
+var socket, activeZsession, zsessionCanceled = false, attachAddon, serializeAddon, term, clientRecordLogFlag,
+    clientRecordLogBlobs = [], CurrentDir = '/root';
+
+
+function parseData(evt) {
+  try {
+    return JSON.parse(evt.data);
+  } catch {
+    return evt.data;
+  }
+}
+
+function sendData(type, data = {}) {
+  window.webshell.sendData(type, data);
+}
+
+
+/**
+ * 进度条绘制
+ * @param term
+ * @param total
+ * @param current
+ */
+function getProgressBar(total, current) {
+  if (total < current) {
+    throw new Error('total must be greater than current');
+  }
+  const progressBarLength = 40;  // 进度条的长度
+  const progress = Math.floor((current / total) * progressBarLength);
+  const empty = progressBarLength - progress;
+  const progressBar = '█'.repeat(progress) + '░'.repeat(empty);
+  return `(${progressBar}) ${((current / total) * 100).toFixed(2)}%`;
+}
+
+async function readFromClipboard() {
+  const doSystemPaste = () => {
+    const textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
+    // 隐藏此输入框
+    textarea.style.position = 'fixed';
+    textarea.style.visibility = 'hidden';
+    textarea.style.top = '10px';
+    // 选中
+    textarea.focus();
+    const res = document.execCommand('paste', null, null);
+    const paste = textarea.value;
+    document.body.removeChild(textarea);
+    if (res) {
+      return paste;
+    }
+    throw new Error();
+  };
+  if (navigator.clipboard) {
+    return navigator.clipboard.readText().catch(() => doSystemPaste());
+  }
+  return doSystemPaste();
+}
+
+async function copyToClipboard(content) {
+  const doSystemCopy = () => {
+    const textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
+    // 隐藏此输入框
+    textarea.style.position = 'fixed';
+    textarea.style.visibility = 'hidden';
+    textarea.style.top = '10px';
+    // 赋值
+    textarea.value = content;
+    // 选中
+    textarea.focus();
+    textarea.select();
+    // 复制
+    const res = document.execCommand('copy', false);
+    // 移除输入框
+    document.body.removeChild(textarea);
+    if (res) {
+      return true;
+    }
+    throw new Error();
+  };
+
+  if (navigator.clipboard) {
+    return navigator.clipboard.writeText(content).then(() => true)
+        .catch(() => doSystemCopy());
+  }
+  return doSystemCopy();
+}
+
+document.querySelector('#recordLogStart').addEventListener('click', recordLogStart);
+document.querySelector('#clientRecordLogStart').addEventListener('click', clientRecordLogStart);
+document.querySelector('#serializeTerminal').addEventListener('click', serializeTerminal);
+document.querySelector('#runCodeServer').addEventListener('click', runCodeServer);
+document.querySelector('#stopCodeServer').addEventListener('click', stopCodeServer);
+document.querySelector('#backgroundOn').addEventListener('click', toggleBackgroundOn);
+document.querySelector('#toggleNewTabOn').addEventListener('click', toggleNewTabOn);
+document.querySelector('#handleSearch').addEventListener('click', handleSearch);
