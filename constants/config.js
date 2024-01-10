593fa5842a30fd024843a7c53aaf319340cadd39
alanhe <qianghe421@gmail.com>
2024-01-10 22:33:11 +0800

    支持workingDirectory配置

--- constants/config.js
+++ constants/config.js
@@ -6,6 +6,7 @@ const connectOpts = {
   keepaliveInterval: 30000,
   keepaliveCountMax: 200,
   readyTimeout: 60 * 1000,
+  _workingDirectory: '/home/ubuntu/123', // 默认打开路径，没有设定的话，则走服务器配置
   // debug: console.log
 };
 
